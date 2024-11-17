192cbb869d9ae674361be4ee5243f3bb54f68d5d
Nikhil Bhnaderi <nikhilbhanderi4410@gmail.com>
2024-11-17 23:58:47 +0530

    bothroute.py

--- bothroute.py
+++ bothroute.py
@@ -3,12 +3,8 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import tkinter as tk
 from tkinter import ttk, messagebox
-
-
 # Step 1: Create a graph object
 metro_graph = nx.Graph()
-
-
 # Define metro stations for both routes
 red_line_stations = [
     "Thaltej Gam", "Thaltej", "Doordarshan Kendra", "Gurukul Road", "Gujarat University",
