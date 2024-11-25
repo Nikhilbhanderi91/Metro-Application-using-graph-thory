d94149d5cc69a63787467170521940bef08f065e
Nikhil Bhnaderi <nikhilbhanderi4410@gmail.com>
2024-11-25 22:00:59 +0530

    bothroute.py

--- bothroute.py
+++ bothroute.py
@@ -26,9 +26,6 @@ blue_line_stations = [
 
 # Add both routes to the graph
 metro_graph.add_nodes_from(red_line_stations + blue_line_stations)
-
-
-
 # Step 3: Define edges (station-to-station connections with distance and time)
 edges = [
     # Red Line edges
@@ -77,22 +74,18 @@ def shortest_time_path(graph, start_station, end_station):
     except nx.NetworkXNoPath:
         messagebox.showinfo("No Path", f"No path found between {start_station} and {end_station}.")
         return []
-
 # Function to get total travel time of the shortest path
 def total_travel_time(graph, path):
     total_time = 0
     for i in range(len(path) - 1):
         total_time += graph[path[i]][path[i + 1]]['time']
     return total_time
-
 # Function to calculate total distance of the shortest path
 def total_distance(graph, path):
     total_distance = 0
     for i in range(len(path) - 1):
         total_distance += graph[path[i]][path[i + 1]]['distance']
     return total_distance
-
-
 
 # Function to calculate and visualize the route
 def calculate_route():
